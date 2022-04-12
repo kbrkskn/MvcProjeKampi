fec572b0ca8e417e3de03c06e6eb7928524bebe6
Kubra <Kubra@DESKTOP-J3V4CEL>
2022-04-12 10:17:51 +0300

    Add project files.

---
+++ DataAccessLayer/Abstract/ICategoryDal.cs
@@ -0,0 +1,27 @@
+﻿using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface ICategoryDal
+    {
+
+        //CRUD
+        //Type Name(); Metotlar böyle tanımlanır
+        List<Category> List();
+
+        void Insert(Category p);//p ile Category sınıfında yer alan propertylere ulaşabileceğim 
+
+        void Update(Category p);
+
+        void Delete(Category p);
+        
+
+        //CRUD operasyonlarının gerçekleşeceği metotları tanımladık
+    }
+}
+ 
---
+++ DataAccessLayer/Concreate/Repositories/CategoryRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concreate.Repositories
+{
+    public class CategoryRepository : ICategoryDal
+    {
+
+
+        Context c=new Context();
+        DbSet<Category> _object;
+        public void Delete(Category p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Category p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> List()
+        {
+         return _object.ToList();
+
+        }
+
+        public void Update(Category p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+/*
+ * 
+ * ToList
+ * Add
+ * 
+ 
+ 
+ */
