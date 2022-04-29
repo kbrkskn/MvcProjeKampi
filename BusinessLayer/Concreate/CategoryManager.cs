2e7cc46131b97dca3da77daae3db6751ee7133ed
Kubra <Kubra@DESKTOP-J3V4CEL>
2022-04-29 14:28:57 +0300

    27.videoya kadar olan kısım

--- BusinessLayer/Concreate/CategoryManager.cs
+++ BusinessLayer/Concreate/CategoryManager.cs
@@ -18,7 +18,7 @@ namespace BusinessLayer.Concreate
         }
         public void CategoryAddBL(Category p)
         {
-            if (p.CategoryName == "" || p.CategoryName.Length <= 3 || p.CategoryStatus == false || p.CategoryName.Length >= 51)
+            if (p.CategoryName == "" || p.CategoryName.Length <= 3  || p.CategoryDescription== "" || p.CategoryName.Length >= 51)
             {
                 //Hata mesajı
             }
--- MvcProjeKampi/Controllers/CategoryController.cs
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using BusinessLayer.Concreate;
-
+using EntityLayer.Concreate;
 
 namespace MvcProjeKampi.Controllers
 {
@@ -20,6 +20,17 @@ namespace MvcProjeKampi.Controllers
         {
             var categoryvalues = cm.GetAllBL();
             return View(categoryvalues);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]//Sayfada bir butona tıklanıldığında, bir şey post edildiğinde burası çalışacak
+        public ActionResult AddCategory(Category p)
+        {
+            cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");//Ekleme işlemini gerçekleştrdikten sonra tekrardan listelemek için
         }
     }
 }
